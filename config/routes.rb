56295efa4f2734874e97cb01c6b957f9c57cde47
Daniel Mitchell <dmitche3@uccs.edu>
2021-05-08 19:01:41 +0000

    Added CSS from Bootstrap and from scratch to make website look nice

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: "projects#index"
-  get "/about", to: "projects#about"
   resources :projects
   resources :students
+ 
 end
